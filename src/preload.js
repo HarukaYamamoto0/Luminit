49bc1297d73af3ccc4e109e3b9467d31b19d646d
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2024-12-30 21:34:06 -0300

    refactore: using import and export

--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,12 @@
-window.addEventListener('DOMContentLoaded', () => {
+// eslint-disable-next-line no-undef
+window.addEventListener("DOMContentLoaded", () => {
   const replaceText = (selector, text) => {
+    // eslint-disable-next-line no-undef
     const element = document.getElementById(selector);
     if (element) element.innerText = text;
   };
 
-  for (const dependency of ['chrome', 'node', 'electron']) {
+  for (const dependency of ["chrome", "node", "electron"]) {
     replaceText(`${dependency}-version`, process.versions[dependency]);
   }
 });
